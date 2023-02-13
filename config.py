31adf41259ebdd40079502c85c18144fcc69f347
liranalb <liran.alb@gmail.com>
2023-02-13 11:08:56 +0200

    fix comments

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 RSS_URL = ""
 KEYWORDS = []
-IGNORE_LIST = [] # the will skip entries 
+IGNORE_LIST = [] # entries containing string from this list will be skiped
 MONITOR_INTERVALS = 60 # monitor cycles in seaconds
 MAIL_ACCOUNT_ADDRESS = ""
 MAIL_ACCOUNT_PASSWORD = "" 
