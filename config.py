aab9c94077b3d8a2a8902496de306fa3853f8461
liranalb <liran.alb@gmail.com>
2023-02-13 11:05:46 +0200

    add config file

---
+++ config.py
@@ -0,0 +1,7 @@
+RSS_URL = ""
+KEYWORDS = []
+IGNORE_LIST = [] # the will skip entries 
+MONITOR_INTERVALS = 60 # monitor cycles in seaconds
+MAIL_ACCOUNT_ADDRESS = ""
+MAIL_ACCOUNT_PASSWORD = "" 
+SOURCE_MAIL = ""
